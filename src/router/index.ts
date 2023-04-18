db4f8fc49bbf425b4a16efa6ed84f2c8a79445b9
igor-paiva <igorbatistapaiva@outlook.com>
2023-04-18 20:25:27 -0300

    Creates not found page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
 import SignUp from '../views/SignUp.vue';
+import NotFound from '../views/NotFound.vue';
 
 export const routes: Array<RouteRecordRaw> = [
     {
@@ -17,6 +18,12 @@ export const routes: Array<RouteRecordRaw> = [
         path: '/signup',
         name: 'SignUp',
         component: SignUp,
+    },
+    {
+        // Always leave this as last one
+        path: '/:pathMatch(.*)',
+        name: 'NotFound',
+        component: NotFound,
     },
 ];
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,68 @@
+<template>
+    <ion-page id="main-content">
+        <ion-header :translucent="true">
+            <ion-toolbar>
+                <ion-buttons slot="start">
+                    <ion-menu-button></ion-menu-button>
+                </ion-buttons>
+
+                <ion-title>Página não encontrada</ion-title>
+            </ion-toolbar>
+        </ion-header>
+
+        <ion-content :fullscreen="true">
+            <div class="container">
+                <ion-text color="medium">
+                    <h2>Não encontrado...</h2>
+                </ion-text>
+
+                <ion-icon
+                    :icon="sad"
+                    color="medium"
+                    class="ion-margin-top sad-icon"
+                ></ion-icon>
+
+                <ion-button
+                    shape="round"
+                    :onclick="() => $router.push({ name: 'Home' })"
+                >
+                    Ir para página inicial
+                </ion-button>
+            </div>
+        </ion-content>
+    </ion-page>
+</template>
+
+<style>
+.container {
+    width: 100%;
+    display: flex;
+    padding-top: 32px;
+    align-items: center;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.sad-icon {
+    width: 64px;
+    height: 64px;
+    margin-bottom: 24px;
+}
+</style>
+
+<script setup lang="ts">
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonText,
+    IonIcon,
+    IonButton,
+    IonButtons,
+    IonMenuButton,
+} from '@ionic/vue';
+
+import { sad } from 'ionicons/icons';
+</script>
