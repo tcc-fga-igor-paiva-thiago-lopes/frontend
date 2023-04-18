a5728467e4b1e57e956777c6c5430b70c305e842
igor-paiva <igorbatistapaiva@outlook.com>
2023-04-18 20:57:41 -0300

    Add tests to welcome page

--- tests/unit/views/home.spec.ts
+++ tests/unit/views/home.spec.ts
@@ -1,36 +1,14 @@
 import { mount } from '@vue/test-utils';
-import { createPinia, setActivePinia } from 'pinia';
 
-import WelcomePage from '@/views/WelcomePage.vue';
+import HomePage from '@/views/HomePage.vue';
 
-beforeAll(() => {
-    setActivePinia(createPinia());
-});
-
-describe('WelcomePage.vue', () => {
+describe('HomePage.vue', () => {
     it('renders home vue', async () => {
-        const wrapper = mount(WelcomePage);
+        const wrapper = mount(HomePage);
 
-        const button = wrapper.find('ion-button');
+        const message = wrapper.find('ion-content>ion-text');
 
-        expect(button.exists()).toBe(true);
-        expect(button.text()).toBe('Criar conta');
-    });
-
-    it('redirects to signup page when button is clicked', async () => {
-        const mockRouter = { push: jest.fn() };
-
-        const wrapper = mount(WelcomePage, {
-            global: {
-                mocks: {
-                    $router: mockRouter,
-                },
-            },
-        });
-
-        await wrapper.get('ion-button').trigger('click');
-
-        expect(mockRouter.push).toHaveBeenCalledTimes(1);
-        expect(mockRouter.push).toHaveBeenCalledWith({ name: 'SignUp' });
+        expect(message.exists()).toBe(true);
+        expect(message.text()).toBe('Veja suas opções...');
     });
 });
