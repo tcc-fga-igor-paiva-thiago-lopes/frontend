589dca261898305385863850dc609be814bce9a7
igor-paiva <igorbatistapaiva@outlook.com>
2023-04-22 18:59:53 -0300

    Fixes home page tests

--- tests/unit/views/home.spec.ts
+++ tests/unit/views/home.spec.ts
@@ -1,14 +1,24 @@
 import { mount } from '@vue/test-utils';
+import { createPinia, setActivePinia } from 'pinia';
 
 import HomePage from '@/views/HomePage.vue';
+import { TestHelper } from '../../testHelper';
+
+beforeAll(async () => {
+    setActivePinia(createPinia());
+
+    await TestHelper.instance.setupTestDB();
+});
+
+afterAll(() => TestHelper.instance.teardownTestDB);
 
 describe('HomePage.vue', () => {
     it('renders home vue', async () => {
         const wrapper = mount(HomePage);
 
-        const message = wrapper.find('ion-content>ion-text');
+        const message = wrapper.find('ion-toolbar>ion-title');
 
         expect(message.exists()).toBe(true);
-        expect(message.text()).toBe('Veja suas opções...');
+        expect(message.text()).toBe('Página inicial');
     });
 });
