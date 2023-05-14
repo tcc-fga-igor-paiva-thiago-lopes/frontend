8d0c60355bf945018d3eeb1ed1ad14d24bcb4092
igor-paiva <igorbatistapaiva@outlook.com>
2023-05-14 15:29:20 -0300

    Redirect when route is not allowed and device is offline

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,10 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+
 import AuthService from '@/services/auth';
+import { useAppStore } from '@/store/app';
+import { presentToast } from '@/utils/toast';
+
 import SignUp from '../views/SignUp.vue';
 import SignIn from '../views/SignIn.vue';
 import HomePage from '@/views/HomePage.vue';
@@ -49,12 +53,28 @@ const router = createRouter({
     routes,
 });
 
+const offlinePermittedRoutes = ['Home', 'NotFound'];
+
 router.beforeEach(async (to) => {
+    const { readNetworkStatus } = useAppStore();
+
+    const connectionStatus = await readNetworkStatus();
+
     if (to.meta.requiresAuth && !(await AuthService.hasToken())) {
         return {
-            path: '/login',
+            name: 'SignIn',
             query: { redirect: to.fullPath },
         };
+    } else if (
+        !connectionStatus.connected &&
+        !offlinePermittedRoutes.includes(to.name as string)
+    ) {
+        presentToast(
+            'Esta página não é permitida sem conexão com a Internet',
+            'danger'
+        );
+
+        return { name: 'Home' };
     }
 });
 
