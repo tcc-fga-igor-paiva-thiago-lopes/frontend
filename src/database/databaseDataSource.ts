78d216ccb248bd46427a070092abdb2f83fa44dd
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2023-06-06 10:43:07 -0300

    Removes Account Store references

--- src/database/databaseDataSource.ts
+++ src/database/databaseDataSource.ts
@@ -1,8 +1,8 @@
 import { DataSource } from 'typeorm';
 
 import sqliteConnection from '@/database';
-import { Account } from '@/models/account';
-import { AddAccountsTable1679179393713 } from '@/database/migrations/1679179393713-AddAccountsTable';
+import { CategoryGroup } from '@/models/categoryGroup';
+import { AddCategoriesGroupsTable1685921479153 } from '@/database/migrations/1685921479153-AddCategoriesGroupsTable';
 
 export type DataSourceType = DataSource;
 
@@ -11,8 +11,8 @@ export default new DataSource({
     type: 'capacitor',
     driver: sqliteConnection,
     database: process.env.VUE_APP_DB_NAME || 'truck-driver-app',
-    entities: [Account],
-    migrations: [AddAccountsTable1679179393713],
+    entities: [CategoryGroup],
+    migrations: [AddCategoriesGroupsTable1685921479153],
     // entities: ['src/models/**/*.ts', '!src/models/appBaseEntity.ts'],
     // migrations: ['src/database/migrations/**/*.ts'],
     logging: ['error', 'query', 'schema'],
