308bed2df368fea13d83396bbe2f16b1b32660d3
igor-paiva <igorbatistapaiva@outlook.com>
2023-07-04 21:00:42 -0300

    Adds placeholder to datetime button

--- src/components/DatetimeButton.vue
+++ src/components/DatetimeButton.vue
@@ -1,14 +1,20 @@
 <template>
-    <ion-datetime-button
-        :datetime="identifier"
-        :disabled="disabled"
-    ></ion-datetime-button>
+    <ion-datetime-button :datetime="identifier" :disabled="disabled">
+        <span slot="date-target" v-if="!value">{{ datePlaceholder }}</span>
+
+        <span slot="time-target" v-if="!value">{{ timePlaceholder }}</span>
+    </ion-datetime-button>
 
     <ion-modal :keep-contents-mounted="true">
         <ion-datetime
             :id="identifier"
             locale="pt-BR"
             :value="value"
+            show-clear-button
+            clear-text="Limpar"
+            done-text="Pronto"
+            cancel-text="Cancelar"
+            show-default-buttons
             @ion-change="(e) => emit('valueChange', e)"
         ></ion-datetime>
     </ion-modal>
@@ -25,10 +31,17 @@ interface IProps {
     identifier: string;
     disabled?: boolean;
     value?: string | null;
+    datePlaceholder?: string;
+    timePlaceholder?: string;
 }
-const props = withDefaults(defineProps<IProps>(), { disabled: false });
+const props = withDefaults(defineProps<IProps>(), {
+    disabled: false,
+    timePlaceholder: 'HH:mm',
+    datePlaceholder: 'Clique para selecionar',
+});
 
-const { value, disabled, identifier } = toRefs(props);
+const { value, disabled, identifier, datePlaceholder, timePlaceholder } =
+    toRefs(props);
 
 const emit = defineEmits(['valueChange']);
 </script>
